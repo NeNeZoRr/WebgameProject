12ebb0a932217465a6a803471821fe2a2693a764
NeNeZoRr <zorrillalin@gmail.com>
2023-10-09 12:12:23 -0400

    Update 9-2 	modified:   Styling.css 	modified:   main.html 	modified:   main.js 	modified:   word.js

--- main.js
+++ main.js
@@ -1,46 +1,67 @@
+// Import the wordArray from word.js
 import wordArray from './word.js';
 
-// DOM elements //
+// DOM elements
 const wordText = document.querySelector(".word");
 const hintText = document.querySelector(".hint span");
-const timerDisplay = document.getElementById("timer");
+const gameTimerDisplay = document.getElementById("game-timer");
 const inputField = document.querySelector("input");
 const refreshBtn = document.querySelector(".new-word");
 const checkBtn = document.querySelector(".verify-word");
-const scoreCounter = document.getElementById("score-counter"); 
+const scoreCounter = document.getElementById("score-counter");
 
-//Game Initialization function//
-let correctWord, timer, gameStarted = false;
-let score = 0; 
+// Game Initialization variables
+let correctWord, gameTimer, gameStarted = false;
+let score = 0;
 
-// Timer function //
-const initTimer = maxTime => {
-    clearInterval(timer);
-    timerDisplay.style.display = "block"; 
-    timer = setInterval(() => {
+// Timer function
+const initGameTimer = maxTime => {
+    clearInterval(gameTimer);
+    gameTimerDisplay.style.display = "block";
+    gameTimer = setInterval(() => {
         if (maxTime > 0) {
             maxTime--;
-            updateTimerDisplay(maxTime);
+            updateGameTimerDisplay(maxTime);
         } else {
-            clearInterval(timer);
-            if (gameStarted) {
-                alert(`Time's up! ${correctWord.toUpperCase()} was the correct word`);
-                initGame();
+            clearInterval(gameTimer);
+            if (score >= 15) {
+                alert("Winner!");
+            } else {
+                alert("Game over");
             }
+            initGame();
         }
     }, 1000);
 };
 
-// Timer function //
-const updateTimerDisplay = remainingTime => {
+// Timer display function
+const updateGameTimerDisplay = remainingTime => {
     const minutes = Math.floor(remainingTime / 60);
     const seconds = remainingTime % 60;
     const formattedTime = `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
-    timerDisplay.innerText = formattedTime;
+    gameTimerDisplay.innerText = formattedTime;
 };
 
-// Init game function & Array Function//
+// Function to initialize the game
 const initGame = () => {
+    wordText.innerText = "";
+    hintText.innerText = "";
+    inputField.value = "";
+    gameStarted = false;
+    score = 0;
+    scoreCounter.innerText = score;
+};
+
+// Event listener for the "New Word" button
+refreshBtn.addEventListener("click", () => {
+    initGame();
+    initGameTimer(180); // 180 seconds = 3 minutes
+    gameStarted = true;
+    generateWord();
+});
+
+// Generate a new word function
+const generateWord = () => {
     let randomObj = wordArray[Math.floor(Math.random() * wordArray.length)];
     let wordArrayShuffled = randomObj.word.split("");
     for (let i = wordArrayShuffled.length - 1; i > 0; i--) {
@@ -50,37 +71,20 @@ const initGame = () => {
     wordText.innerText = wordArrayShuffled.join("");
     hintText.innerText = randomObj.hint;
     correctWord = randomObj.word.toLowerCase();
-    inputField.value = "";
-    inputField.setAttribute("maxlength", correctWord.length);
-    gameStarted = true; 
-    initTimer(30);
 };
 
-//Word checking function//
-const checkWord = () => {
+// Event listener for the "Verify Word" button
+checkBtn.addEventListener("click", () => {
     let userWord = inputField.value.toLowerCase();
     if (!userWord) return alert("Please enter the word to check!");
-    if (userWord !== correctWord) return alert(`Oops! ${userWord} is not the correct word`);
-    clearInterval(timer);
-    alert(`Congrats! ${correctWord.toUpperCase()} is the correct word`);
-
-// Score incrementing function//
-    score++;
-    scoreCounter.innerText = score;
-
-    initGame();
-};
-
-// New Word Action //
-refreshBtn.addEventListener("click", () => {
-    gameStarted = false;
-    clearInterval(timer);
-    timerDisplay.style.display = "none";
-    initGame();
+    if (userWord !== correctWord) {
+        alert(`Oops! ${userWord} is not the correct word`);
+    } else {
+        score++;
+        scoreCounter.innerText = score;
+    }
+    generateWord();
 });
 
-// Check Word Action //
-checkBtn.addEventListener("click", checkWord);
-
-//Timer Init Display// 
-timerDisplay.style.display = "none";
+// Initially hide the game timer
+gameTimerDisplay.style.display = "none";
